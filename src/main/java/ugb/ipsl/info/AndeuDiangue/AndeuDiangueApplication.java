1b7d4facd565c1479029279bc4fd5c719470d923
Jerome-Sadio <jeromesadio2020@gmail.com>
2026-02-21 22:50:37 +0000

    first commit

---
+++ src/main/java/ugb/ipsl/info/AndeuDiangue/AndeuDiangueApplication.java
@@ -0,0 +1,13 @@
+package ugb.ipsl.info.AndeuDiangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndeuDiangueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndeuDiangueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ugb/ipsl/info/AndeuDiangue/AndeuDiangueApplicationTests.java
@@ -0,0 +1,13 @@
+package ugb.ipsl.info.AndeuDiangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndeuDiangueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
