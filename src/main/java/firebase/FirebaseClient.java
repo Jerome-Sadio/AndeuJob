b2bdb3467d022b693e80c31325f8bf470512c7d1
jeanthiao <thiao.jean-christophe-emmanuel@ugb.edu.sn>
2026-02-25 22:38:32 +0000

    first push

--- src/main/java/firebase/FirebaseClient.java
+++ src/main/java/firebase/FirebaseClient.java
@@ -1,4 +1,12 @@
 package firebase;
+import dto.NotificationRequestDTO;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FirebaseClient {
+    public void envoyer(NotificationRequestDTO request) {
+
+        System.out.println("Envoi Firebase → " + request.getDestinataire());
+        System.out.println(request.getTitre() + " : " + request.getMessage());
+    }
 }
